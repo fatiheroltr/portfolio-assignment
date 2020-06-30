dea40ab1f37e7cdb6d868a04bbcc7bca0abc8881
fatiheroltr <fatiheroltr@gmail.com>
2020-06-30 17:09:51 -0400

    String varables created

---
+++ Scripts/app.js
@@ -0,0 +1,46 @@
+
+let jumbo1="Hi there!";
+document.getElementById("jumbo-1").innerHTML = jumbo1;
+let jumbo2="My name is Fatih Erol. A brand new father, devoted husband,graphic designer, developer candidate, audiophile, nature lover";
+document.getElementById("jumbo-2").innerHTML = jumbo2;
+let jumbo3="Would you like to learn more about me?";
+document.getElementById("jumbo-3").innerHTML = jumbo3;
+let menu1=" About";
+document.getElementById("menu-1").innerHTML = menu1;
+let menu2=" Projects";
+document.getElementById("menu-2").innerHTML = menu2;
+let menu3=" Contact";
+document.getElementById("menu-3").innerHTML = menu3;
+let card1_title="Brand new father";
+document.getElementById("card1-title").innerHTML = card1_title;
+let card1_text="It is the most beautiful thing ever happened to me. Also the hardest task.";
+document.getElementById("card1-text").innerHTML = card1_text;
+let card2_title="Graphic designer";
+document.getElementById("card2-title").innerHTML = card2_title;
+let card2_text="I am a graphic designer for more than a decade. Still learning and sharing the experience.";
+document.getElementById("card2-text").innerHTML = card2_text;
+let card3_title="Developer candidate";
+document.getElementById("card3-title").innerHTML = card3_title;
+let card3_text="Currently in training and have a big appetite for Python. They are right, code is poetry.";
+document.getElementById("card3-text").innerHTML = card3_text;
+let about_h2="About me";
+document.getElementById("about-h2").innerHTML = about_h2;
+let projects_h2="Projects";
+document.getElementById("projects-h2").innerHTML = projects_h2;
+let project1_title="Sudoku Solver";
+document.getElementById("project1-title").innerHTML = project1_title;
+let project1_text="This is a C# console application for practicing 'Backtracking'";
+document.getElementById("project1-text").innerHTML = project1_text;
+let project2_title="BringIt";
+document.getElementById("project2-title").innerHTML = project2_title;
+let project2_text="It is an app for food delivery in campus. It started as an assignment but it is progressing well";
+document.getElementById("project2-text").innerHTML = project2_text;
+let project3_title="HackerRank";
+document.getElementById("project3-title").innerHTML = project3_title;
+let project3_text="Trying to create a profile that I can show my coding abilities";
+document.getElementById("project3-text").innerHTML = project3_text;
+
+
+
+
+
